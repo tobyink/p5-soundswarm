3bd20334936a07580281e4f949f4d332b3037e14
Toby Inkster <mail@tobyinkster.co.uk>
2013-01-25 07:23:22 +0000

    add strict and warnings; no Async.pm
    --HG--
    branch : use-coro

--- lib/SoundSwarm/Syntax.pm
+++ lib/SoundSwarm/Syntax.pm
@@ -1,8 +1,10 @@
 package SoundSwarm::Syntax;
 
-use Async ();
-use SoundSwarm;
+use SoundSwarm;  # defines constants
 use Syntax::Collector -collect => q/
+	use strict 0;
+	use warnings 0;
+	no warnings 0 qw( numeric void uninitialized once );
 	use Carp 0 qw( confess );
 	use Scalar::Util 0 qw( blessed );
 	use MooX::Types::MooseLike::Base 0 qw( :all );
